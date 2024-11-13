24b78e7b2e5895f72fcb8a497ce2c664717f7f33
Elena <ek28150@gmail.com>
2024-11-12 18:09:54 -0800

    corrected the last lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,38 @@
 import * as React from 'react';
-const list = [
-  {
-  title: 'React',
-  url: 'https://reactjs.org/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
+
+
+const  App = () => {
+  const stories = [
+    {
+    title: 'React',
+    /*url: 'https://reactjs.org/',*/
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+    },
+    {
+    title: 'me',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
   },
-  {
-  title: 'me',
-  url: 'https://redux.js.org/',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1,
-},
-];
-const  App = () => (
+  ];
+  return(
   <div>
   <h1>My Hacker Stories</h1>
   <h2>It is {new Date().toLocaleTimeString()}.</h2>
   <Search />
   <hr />
-  <List />
+  <List list = {stories}/>
   </div>
 );
-  
-
-const Search = () => (
-<div>
-<label htmlFor="search">Search: </label>
-<input id="search" type="text" />
-</div>
-);
-
-const List = () => (
+}
+const List = (props) => (
   <ul>
-  {list.map((item) => (
+  {props.list.map((item) => (
   <li key={item.objectID}>
   <span>
   <a href={item.url}>{item.title}</a>
@@ -49,5 +44,22 @@ const List = () => (
   ))}
   </ul>
   );
+  const Search = () => {
+
+    const handleChange = (event) => {
+    
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
+    }
+
+  return (
+<div>
+<label htmlFor="search">Search: </label>
+<input id="search" type="text" onChange={handleChange} />
+</div>
+  );
+    }
   
 export default App;
