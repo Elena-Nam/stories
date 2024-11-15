fb592dc6103471948b32c9b34e0290b47a8220c6
Elena <ek28150@gmail.com>
2024-11-14 19:11:16 -0800

    added props, state and callback handler

--- src/App.jsx
+++ src/App.jsx
@@ -20,46 +20,60 @@ const  App = () => {
     objectID: 1,
   },
   ];
+
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+  };
+
   return(
   <div>
   <h1>My Hacker Stories</h1>
   <h2>It is {new Date().toLocaleTimeString()}.</h2>
-  <Search />
+
+  <Search onSearch = {handleSearch}/>
   <hr />
+
   <List list = {stories}/>
   </div>
 );
 }
+
 const List = (props) => (
   <ul>
   {props.list.map((item) => (
-  <li key={item.objectID}>
-  <span>
-  <a href={item.url}>{item.title}</a>
-  </span>
-  <span>{item.author}</span>
-  <span>{item.num_comments}</span>
-  <span>{item.points}</span>
-  </li>
+  <Item key={item.objectID} item = {item}/>
   ))}
   </ul>
   );
-  const Search = () => {
+  const Item = (props) => (
+    <li>
+    <span>
+    <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+    </li>
+  );
 
+  const Search = (props) => {
+    const [searchTerm, setSearchTerm] = React.useState('');
     const handleChange = (event) => {
     
-    // synthetic event
-    console.log(event);
-    // value of target (here: input HTML element)
-    console.log(event.target.value);
+   setSearchTerm(event.target.value);
+   props.onSearch(event);
     }
 
   return (
-<div>
-<label htmlFor="search">Search: </label>
-<input id="search" type="text" onChange={handleChange} />
-</div>
+  <div>
+  <label htmlFor="search">Search: </label>
+  <input id="search" type="text" onChange={handleChange} />
+
+  <p>
+  Searching for <strong>{searchTerm}</strong>.
+  </p>
+  </div>
   );
-    }
+}
   
 export default App;
