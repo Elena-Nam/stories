5ff19c9f19fb0e6856ee17569f5b70f851d98662
Elena <ek28150@gmail.com>
2025-01-02 18:29:53 -0800

    changed from client-side search into server-side search, deleted searchStories variable, changed useEffect with searchTerm

--- src/App.jsx
+++ src/App.jsx
@@ -91,8 +91,11 @@ const  App = () => {
       }, []);
   */
   React.useEffect(() => {
+    if (!searchTerm) return;
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
-    fetch(`${API_ENDPOINT}react`)
+
+    fetch(`${API_ENDPOINT}${searchTerm}`)
     .then((response) => response.json())
     .then((result) => {
       dispatchStories({
@@ -103,7 +106,7 @@ const  App = () => {
       .catch(() =>
       dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
       );
-      }, []);
+      }, [searchTerm]);
   
 
   const handleRemoveStory = (item) => {
@@ -116,9 +119,9 @@ const  App = () => {
   const handleSearch = (event) => {
     setSearchTerm (event.target.value);
   };
-
+/*
   const searchedStories = stories.data.filter ((story) => story.title.toLowerCase().includes (searchTerm.toLowerCase()));
-  
+  */
   return (
   <div>
     <h1>My Hacker Stories</h1>
@@ -136,7 +139,7 @@ const  App = () => {
 
     {stories.isError && <p> Something went wrong ... </p>}
     {stories.isLoading ? ( <p>Loading ...</p> ) : (
-    <List list = {searchedStories} onRemoveItem = {handleRemoveStory} />
+    <List list = {stories.data} onRemoveItem = {handleRemoveStory} />
     )}
 
   </div>
