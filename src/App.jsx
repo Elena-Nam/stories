04add739b99532b6614c40699797ef8821082390
Elena <ek28150@gmail.com>
2025-01-04 19:59:45 -0800

    changed from impicit to explicit data fetching, added a button to input field, created a new state with url, and 2 functions handleSearchInput and handleSearchSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -49,15 +49,15 @@ const  App = () => {
   const [searchTerm, setSearchTerm] = useStorageState (
     'search','React');
 
+    const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
+
   const [stories, dispatchStories] = React.useReducer (storiesReducer, { data: [], isLoading: false, isError: false });
   
   const handleFetchStories = React.useCallback (() => {
 
-    if (!searchTerm) return;
-
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(`${API_ENDPOINT}${searchTerm}`)
+    fetch(url)
     .then((response) => response.json())
     .then((result) => {
       dispatchStories({
@@ -68,11 +68,11 @@ const  App = () => {
       .catch(() =>
       dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
       );
-      }, [searchTerm]);
+      }, [url]);
   
   React.useEffect(() => {
     handleFetchStories();
-  }, []);
+  }, [handleFetchStories]);
 
   const handleRemoveStory = (item) => {
     dispatchStories ({
@@ -81,9 +81,14 @@ const  App = () => {
     });
   };
       
-  const handleSearch = (event) => {
-    setSearchTerm (event.target.value);
-  };
+  
+const handleSearchInput = (event) => {
+setSearchTerm(event.target.value);
+};
+
+const handleSearchSubmit = () => {
+setUrl(`${API_ENDPOINT}${searchTerm}`);
+};
 
   return (
   <div>
@@ -94,10 +99,10 @@ const  App = () => {
     id = "search"
     value = {searchTerm}
     isFocused
-    onInputChange = {handleSearch}>
+    onInputChange = {handleSearchInput}>
     <strong> Search: </strong>
     </InputWithLabel>
-  
+    <button type ="button" disabled = {!searchTerm} onClick = {handleSearchSubmit}> Submit </button>
     <hr/>
 
     {stories.isError && <p> Something went wrong ... </p>}
